06ce6cb51d70ba8e2b762da6e0ea48cb37fee17b
PRESTEBAN <145607969+PRESTEBAN@users.noreply.github.com>
2025-07-10 08:20:08 -0500

    first commit

---
+++ src/Models/Graph.java
@@ -0,0 +1,45 @@
+package Models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Graph {
+    
+    Set<Node> nodes = new HashSet<>();
+
+    public Graph() {
+    }
+
+    public Node addNode(int value){
+        return null;
+    }
+
+    public void addEdge(Node src, Node dest){
+
+    }
+    
+    public void printGraph(){
+
+    }
+
+    public void getDFS(Node startNode){
+
+    }
+
+    private void getDFSUtil(Node node, boolean[] visited){
+
+    }
+
+    public void getBFS(Node startNode){
+
+    }
+
+    public int[][] getAdjacencyMatrix(){
+        return null;
+    }
+
+    public void printAdjacencyMatrix(){
+        
+    }
+
+}
