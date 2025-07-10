55b5aee3650d8c601fba8822ac1a8663774f3d61
PRESTEBAN <145607969+PRESTEBAN@users.noreply.github.com>
2025-07-10 08:27:05 -0500

    inicio

--- src/Models/Node.java
+++ src/Models/Node.java
@@ -1,6 +1,28 @@
 package Models;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Node {
     private int value;
+    private Set<List<Node>> neighbors;
     
+    public Node(int value){
+        this.value = value;
+        this.neighbors  = new HashSet<>();
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Set<List<Node>> getNeighbors() {
+        return neighbors;
+    }
+
+     
+    public void addNeighbor(Node neighbor){
+
+    }
 }
